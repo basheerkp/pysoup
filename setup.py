38901f2ebd6b0693bf73bca442122ea848c5f667
basheerkp <basheerahmadkp@gmail.com>
2024-12-08 13:03:26 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pysoup',
+    version='1.0',
+    packages=[''],
+    url='github.com/basheerkp/pysoup',
+    license='',
+    author='ahmed',
+    author_email='',
+    description=''
+)
